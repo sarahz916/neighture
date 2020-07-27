9cb25bf927ac05d1d12e2ab178814487eb512839
Andrea <acoll3@stanford.edu>
2020-07-27 15:23:23 +0000

    Added function in route-script.js to print out waypoints and their user inputted labels in their route order.

--- src/main/webapp/route-script.js
+++ src/main/webapp/route-script.js
@@ -16,42 +16,97 @@ window.onload = function setup() {
     // Inialize and create a map with no directions on it when the page is reloaded.
     var chicago = new google.maps.LatLng(41.850033, -87.6500523); // hardcoded start; will get from user later
     initMap(chicago);
-    console.log('reload');
 }
 
-document.getElementById('form').addEventListener('submit', createMapWithWaypoint());
+document.getElementById('form').addEventListener('submit', createMapWithWaypoints());
 
 /**
  * Create a route and map from a waypoint entered by the user.
  */
-async function createMapWithWaypoint() {
-    let res = await getWaypoints();
-    console.log(res);
-    let waypoints = [];
-    for (let pt of res) {
-        let waypoint = new google.maps.LatLng(pt.y, pt.x);
-        waypoints.push(waypoint);
-    }
+async function createMapWithWaypoints() {
+    var res = await getWaypoints();
+    let waypoints = convertWaypointstoLatLng(res);
     let start = new google.maps.LatLng(41.850033, -87.6500523); // hardcoded start; will get from user later
     let end = new google.maps.LatLng(41.850033, -87.5500523); // hardcoded end; will get from user later
-    createMapWithDirections(start, end, waypoints);
-
-}
-
-/**
- * Given a start coordinate, end coordinate, and a list of waypoint coordinates, 
- * create a route and a map and display the route on the map to the user.
- */
-function createMapWithDirections(start, end, waypoints) {
     let map = initMap(start);
     let directionsService = new google.maps.DirectionsService();
     let directionsRenderer = new google.maps.DirectionsRenderer({
-        map: map,
-        draggable: true
+        map: map
     });
     calcRoute(directionsService, directionsRenderer, start, end, waypoints);
     generateURL (start, end, waypoints);
-    generateURL(start, end, waypoints)
+}
+
+/**
+ * Given a DirectionsService object, a DirectionsRenderer object, start/end coordinates and a list
+ * of waypoint coordinates, generate a route using the Google Maps API.
+ */
+function calcRoute(directionsService, directionsRenderer, start, end, waypoints) {
+    var waypointsWithLabels = waypoints;
+    let waypointsData = [];
+    Object.entries(waypoints).forEach(pt => waypointsData.push({ location: pt[1] }));
+    let request = {
+        origin: start,
+        destination: end,
+        waypoints: waypointsData,
+        optimizeWaypoints: true,
+        travelMode: 'WALKING'
+    };
+    directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+            directionsRenderer.setDirections(result);
+            createWaypointLegend(result.routes[0], waypointsWithLabels);
+        } else {
+            alert(`Could not display directions: ${status}`);
+        }
+    });
+}
+
+/**
+ * Given a generated route and a JSON object containing waypoint locations with their labels as inputted
+ * by the user, create a legend that maps a marker on the map to corresponding user input.
+ */
+async function createWaypointLegend(route, waypointsWithLabels) {
+    let start = route.legs[0].start_location;
+    console.log('1: start');
+    let i;
+    for (i = 0; i < route.legs.length - 1; i++) {
+        let pt = route.legs[i].end_location;
+        let label = getLabelFromLatLng(pt, waypointsWithLabels);
+        console.log(`${i + 2}: ${label}`);
+    }
+    let end = route.legs[route.legs.length - 1].end_location;
+    console.log(`${i + 2}: end`);
+}
+
+/**
+ * Given a Google Maps LatLng object and JSON containing waypoint coords with labels, 
+ * return the label matching the given LatLng object.
+ */
+function getLabelFromLatLng(pt, waypointsWithLabels) {
+    for (let [label, waypoint] of Object.entries(waypointsWithLabels)) {
+        // Calculate the difference between the lat/long of the points and 
+        // check if its within a certain range.
+        let latDiff = Math.abs(waypoint.lat() - pt.lat());
+        let lngDiff = Math.abs(waypoint.lng() - pt.lng());
+        const range = 0.001;
+        if (latDiff < range && lngDiff < range) {
+            return label;
+        }
+    }
+    return '';
+}
+
+/**
+ * Convert waypoints in JSON form returned by servlet to Google Maps LatLng objects.
+ */
+function convertWaypointstoLatLng(waypoints) {
+     let latlngWaypoints = {};
+     for (let pt of waypoints) {
+        let waypoint = new google.maps.LatLng(pt.y, pt.x);
+        latlngWaypoints[pt.label] = waypoint;
+    }
+    return latlngWaypoints;
 }
 
 /**
@@ -75,36 +130,13 @@ function initMap(center) {
 }
 
 /**
- * Given a DirectionsService object, a DirectionsRenderer object, start/end coordinates and a list
- * of waypoint coordinates, generate a route using the Google Maps API.
- */
-function calcRoute(directionsService, directionsRenderer, start, end, waypoints) {
-    let waypointsData = [];
-    waypoints.forEach(pt => waypointsData.push({ location: pt }));
-    let request = {
-        origin: start,
-        destination: end,
-        waypoints: waypointsData,
-        optimizeWaypoints: true,
-        travelMode: 'WALKING'
-    };
-    directionsService.route(request, function(result, status) {
-        if (status == 'OK') {
-            directionsRenderer.setDirections(result);
-        } else {
-            alert(`Could not display directions: ${status}`);
-        }
-    });
-}
-
-/**
  * Creates a URL based on Maps URLs that will open the generated route on Google Maps on any device.
  */
 function generateURL(start, end, waypoints){
     let globalURL = 'https://www.google.com/maps/dir/?api=1';
     globalURL = globalURL + '&origin=' + start + '&destination=' + end;
     globalURL += '&waypoints='
-    waypoints.forEach(pt => globalURL += pt + '|')
+    Object.entries(waypoints).forEach(pt => globalURL += pt + '|')
     globalURL = globalURL + '&travelmode=walking';
     const URLcontainer = document.getElementById('globalURL');
     globalURL = globalURL.split(" ").join("") //need to get rid of white space for link to work
