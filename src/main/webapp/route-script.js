b0c6d4282cdf3b533c0888b72e60382ae479561a
Andrea <acoll3@stanford.edu>
2020-08-07 19:23:16 +0000

    Changed response format of StartEndServlet to a JSONObject of strings mapped to JSONObjects to access the coordinates and label.

--- src/main/webapp/route-script.js
+++ src/main/webapp/route-script.js
@@ -23,13 +23,17 @@ const MAX_WAYPOINTS = 25;
 window.onload = async function setup() {
     // Initialize and create a map with no directions on it when the page is reloaded.
     var chicago = new google.maps.LatLng(41.850033, -87.6500523); // hardcoded start; will get from user later
-    let start = await getStartAddr();
-    let end = await getEndAddr();
-    // initMap(start, 'route-map');
-    // initMap(start, 'point-map');
-    initMap(chicago, 'route-map');
-    initMap(chicago, 'point-map');
-    await initStartEndDisplay(start, end);
+    let startAddr = await getStartAddr();
+    let endAddr = await getEndAddr();
+
+    let startCoord = await getStartCoord();
+    let endCoord = await getEndCoord();
+
+    initMap(startCoord, 'route-map');
+    initMap(startCoord, 'point-map');
+    // initMap(chicago, 'route-map');
+    // initMap(chicago, 'point-map');
+    await initStartEndDisplay(startAddr, endAddr);
 }
 
 document.getElementById('text-git statform').addEventListener('submit', setupUserChoices());
@@ -44,11 +48,27 @@ async function initStartEndDisplay(start, end) {
 }
 
 /**
+ * Get the start location coordinate in LatLng entered by the user.
+ */
+async function getStartCoord() {
+    let startEnd = await getStartEnd();
+    return new google.maps.LatLng(startEnd.start.y, startEnd.start.x);
+}
+
+/**
+ * Gets the end location coordinate in LatLng entered by the user.
+ */
+async function getEndCoord() {
+    let startEnd = await getStartEnd();
+    return new google.maps.LatLng(startEnd.end.y, startEnd.end.x);
+}
+
+/**
  * Get the start location address entered by the user.
  */
 async function getStartAddr() {
     let startEnd = await getStartEnd();
-    return startEnd.start;
+    return startEnd.start.label;
 }
 
 /**
@@ -56,7 +76,7 @@ async function getStartAddr() {
  */
 async function getEndAddr() {
     let startEnd = await getStartEnd();
-    return startEnd.end;
+    return startEnd.end.label;
 }
 
 /**
@@ -77,8 +97,8 @@ async function createMapWithWaypoints() {
     // let start = new google.maps.LatLng(41.850033, -87.6500523); // hardcoded start; will get from user later
     // let end = new google.maps.LatLng(41.866940, -87.607105); // hardcoded end; will get from user later
 
-    let start = await getStartAddr();
-    let end = await getEndAddr();
+    let start = await getStartCoord();
+    let end = await getEndCoord();
 
     let map = initMap(start, 'route-map');
     let directionsService = new google.maps.DirectionsService();
